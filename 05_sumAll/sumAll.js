f7631fd184f367be67e390f90248d1fdf0d52e3f
Calvin Chien <calvin2chien@gmail.com>
2023-06-10 22:39:29 -0400

    Add 05 solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0;
+    if ((start < 0 || end < 0) ||
+        (typeof start != "number" || typeof end != "number")){
+        return "ERROR";
+    }
+    
+    if (start > end){
+        let start_save = start;
+        start = end;
+        end = start_save;
+    }
 
+    for (let num = start; num <= end; num++) {
+        sum += num;
+    }
+    return sum;
 };
+
+console.log(sumAll(10,"7"));
+//console.log(typeof 10);
 
 // Do not edit below this line
 module.exports = sumAll;
