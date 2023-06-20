5492cc29e1154fbec066fd7060d2e10021d54b73
Calvin Chien <calvin2chien@gmail.com>
2023-06-20 19:11:43 -0400

    Add 08-Calculator solution.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const summation = array.reduce((total,number) => total + number, 0);
+  return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product = array.reduce((total,number) => total * number, 1);
+  return product;
 };
 
-const power = function() {
-	
+console.log('multiply ' + multiply([1,2,3,4]));
+
+const power = function(a,b) {
+  result = a;
+	for (let i = 2; i <= b; i++){
+    result = result * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+console.log('power ' + power(4,3));
+
+const factorial = function(a) {
+	product = 1;
+  while (a > 0){
+    product = product * a;
+    a --;
+  }
+  return product;
 };
+
+console.log('factorial ' + factorial(4));
+
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +49,4 @@ module.exports = {
   power,
   factorial
 };
+
