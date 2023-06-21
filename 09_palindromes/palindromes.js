0902e6f12ed4d1809fa8c7080ac4e8cd5eddb227
Calvin Chien <calvin2chien@gmail.com>
2023-06-20 22:13:03 -0400

    Add 09-palindromes solution.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // assign simpleString first since .reverse() is destructive
+    const simpleArray = string.toUpperCase().split("").filter(x => x.charCodeAt() >= 48);
+    const simpleString = simpleArray.join("");
+
+    const reverseArray = simpleArray.reverse();
+    const reverseString = reverseArray.join("");
+    
+    console.log(simpleString + "__" + reverseString);
+    return simpleString == reverseString;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
