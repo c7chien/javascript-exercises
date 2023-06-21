3e438b4f25ff641db534c267aaef900eb9271264
Calvin Chien <calvin2chien@gmail.com>
2023-06-20 22:59:14 -0400

    Add 10-fibonacci solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    if (sequence < 0) return "OOPS";
+    if (sequence == 0) return 0;
+    let sumArray = [0,1];
+    for (let i = 1; i < sequence; i++){
+        let lastNum = sumArray[sumArray.length-1];
+        let secondNum = sumArray[sumArray.length-2];
+        sumArray.push(lastNum+secondNum);
+        //console.log(sumArray);
+    }
+    return sumArray[sumArray.length-1];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
